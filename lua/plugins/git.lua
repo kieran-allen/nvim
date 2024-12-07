ecf6d7301dd993639a0ff2de1d2c83d9ea4dbd3c
Kieran Allen <kieran.allen@ledger.fr>
2024-12-07 20:10:09 +0000

    update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,1 +1,17 @@
-return {}
+return {
+  "nvim-lua/plenary.nvim",
+  "sindrets/diffview.nvim",
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "linrongbin16/fzfx.nvim",
+    },
+    config = true,
+    keys = {
+      { "<leader>ng", "<cmd>Neogit<cr>", desc = "Neogit" },
+      { "<leader>nc", "<cmd>Neogit commit<cr>", desc = "Neogit commit" },
+    },
+  }
+}
