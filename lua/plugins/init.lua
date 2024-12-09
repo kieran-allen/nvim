28e5ba5cd26b23bf22f0ad6778ffe2c69be6b765
Kieran Allen <kieranallen@proton.me>
2024-12-09 16:07:47 +0000

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "rust", "typescript" },
+          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "rust", "typescript", "markdown", "markdown-inline" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },
