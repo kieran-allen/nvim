ad4b4b89992512de38f06b7b07f5f5f66947f9e7
Kieran Allen <kieran.allen@ledger.fr>
2024-12-13 09:43:34 +0000

    update

--- lua/plugins/fzfx.lua
+++ lua/plugins/fzfx.lua
@@ -9,12 +9,13 @@ return {
     "linrongbin16/fzfx.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons", 'junegunn/fzf' },
     keys = {
-        { "<leader>fa", "<cmd>FzfxFiles<cr>", desc = "Fuzzy search files" },
-        { "<leader>ff", "<cmd>FzfxGFiles<cr>", desc = "FzfxGFiles" },
-        { "<leader>fg", "<cmd>FzfxGLiveGrep<cr>", desc = "FzfxGLiveGrep" },
-        { "<leader>fb", "<cmd>FzfxBufLiveGrep<cr>", desc = "FzfxBufLiveGrep" },
-        { "<leader>FG", "<cmd>FzfxLiveGrep<cr>", desc = "FzfxLiveGrep" },
+        { "<leader>fa", "<cmd>FzfxFiles<cr>", desc = "Find all files" },
+        { "<leader>ff", "<cmd>FzfxGFiles<cr>", desc = "Find git commited files" },
+        { "<leader>fg", "<cmd>FzfxGLiveGrep<cr>", desc = "Find git grep in file" },
+        { "<leader>fb", "<cmd>FzfxBufLiveGrep<cr>", desc = "Find in buffer" },
+        { "<leader>FG", "<cmd>FzfxLiveGrep<cr>", desc = "Find in file" },
         { "<leader>FB", "<cmd>FzfxGBranches<cr>", desc = "Find git branches" },
+        { "<leader>FL", "<cmd>FzfxGBlame<cr>", desc = "Find git blame" },
     },
     config = function()
       require("fzfx").setup()
